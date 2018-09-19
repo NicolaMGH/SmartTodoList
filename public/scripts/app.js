6cbe4845e03f99a9b16506c79425d72dc96626db
Nicola <va3nky@hotmail.com>
2018-09-19 21:50:06 +0000

    implemented ajax request in add.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,4 +9,60 @@
 //   });;
 // });
 
+function createTDL(obj) {
+  const $todo = $("<div>").addClass("lists");
+  const header = `<div class="list-title">
+                    <h2>${obj.title}</h2>
+                    <span id="plus"><i class="fas fa-plus"></i></span>
+                  </div>`;
+  const $bodyCon = $("<div>");
+  for (key in obj) {
+    if (key != "title") {
+      const $body = $(`<div>
+                      <h3>${key}</h3>
+                    </div>`);
+      const $list = $(`<ul class="cat"></ul>`)
+      obj[key].forEach(item => $list.append(`<li>${item}</li>`));
+      $body.append($list);
+      $bodyCon.append($body);
+    }
+  }
 
+  $todo.append(header);
+  $todo.append($bodyCon);
+
+  console.log($todo);
+  return $todo;
+}
+
+function renderTDL(a) {
+    a.forEach(x => $('.todos').append(createTDL(x)))
+}
+
+
+function signInButton (){
+  $('.sign-in').on('click', () => {
+    $.ajax('/api/lists/user_lists', {method: 'GET'})
+      .then((data) => {
+        console.log(data);
+        const d = JSON.parse(data);
+        $('section').append(createTDL(d));
+      })
+  });
+}
+
+
+// const test = JSON.parse('{"title":"foring list","book":["angers","man"]}');
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  // $('section').append(createTDL(test));
+  signInButton();
+});
